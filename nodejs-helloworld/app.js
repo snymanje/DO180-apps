fe1a768712677d4694fc84f262a1ebc0a4692631
Jean Snyman <44743839+snymanje@users.noreply.github.com>
2023-02-19 08:14:38 +0200

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hallo Jean!');
+  res.send('Hallo Jean!!');
 });
 
 app.listen(8080, function () {
