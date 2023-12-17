952059d34a7d0692021f9745655b5c43c0bf179b
Jean Snyman <jean.snyman@gmail.com>
2023-12-17 15:10:56 +0200

    updated app

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -1,13 +1,14 @@
-var createError = require('http-error');
-
-var express = require('express');
+var express = require("express");
 app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.environment.HOSTNAME + '\n')
+app.get("/", function (req, res) {
+  res.send("Hello World from pod: " + process.environment.HOSTNAME + "\n");
+});
+
+app.get("/hello", function (req, res) {
+  res.send("Hello World");
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log("Example app listening on port 8080!");
 });
-
